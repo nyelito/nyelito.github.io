ab583e7bac2b3c46ecf7ad2c5e0888518c3b3c24
Nick Yelito <nyelito@users.noreply.github.com>
2016-08-31 12:37:59 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
-console.log('This would be the main JS file.');
+var restApp = angular.module("restApp", []);
+
+  restApp.controller('GithubController', function($scope, $http){
+    $http.get("https://api.github.com/users/nyelito")
+      .success(function(data, status){
+        $scope.githubInfo = data;
+      })
+      .error(function(data, status){
+        console.log('Unable to retrieve user');
+      });
+  });
